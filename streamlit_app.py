b1ae686953bfeb771983977993ff62be12c237b2
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2024-12-17 14:20:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ import pinecone
 import uuid
 import os 
 os.environ["PINECONE_API_KEY"] = "pcsk_6pU2by_7RqfcYiJdc3QoZJVmtqLjBZWZzABszayaXF6fVRJ47pEaKrDu8XZKAsKHZPTrmw"
-pinecone.init()
+try:
+    pinecone.init()
 index_name = 'textembedding'
-
 vectors_to_insert = [
     ("vec1", np.random.rand(128).tolist()), 
     ("vec2", np.random.rand(128).tolist()), 
