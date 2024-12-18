b44ee211b49b240a299209d9685babfd70c4d8a0
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2024-12-18 11:16:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,17 @@ import fitz
 from sentence_transformers import SentenceTransformer
 import pinecone
 import os
-import huggingface_hub 
 
 os.environ['HUGGINGFACE_API_KEY'] = st.secrets["HUGGINGFACE_API_KEY"]
 os.environ['PINECONE_API_KEY'] = st.secrets["PINECONE_API_KEY"]
 
+pinecone.init(api_key=os.environ["PINECONE_API_KEY"], environment="us-west1-gcp")
 
+index_name = "pdf-embeddings"
+if index_name not in pinecone.list_indexes():
+    pinecone.create_index(index_name, dimension=768)
+
+index = pinecone.Index(index_name)
 
 class PDFLoader:
     def __init__(self, pdf_file):
@@ -26,13 +31,14 @@ class EmbeddingGenerator:
     def __init__(self, model_name='all-MiniLM-L6-v2'):
         self.model = SentenceTransformer(model_name)
 
-    def generate_embeddings(self, text):
-        return self.model.encode(text)
+    def generate_embeddings(self, text_chunks):
+        return self.model.encode(text_chunks)
+
 
 def store_embeddings(embeddings, metadata):
     for i, embedding in enumerate(embeddings):
-        index.upsert([(f'doc-{i}', embedding, metadata[i])])
-
+        id = f'doc-{i}'
+        index.upsert([(id, embedding, metadata[i])])
 
 st.title("PDF Embedding Generator")
 
@@ -42,13 +48,12 @@ if uploaded_file is not None:
     pdf_loader = PDFLoader(uploaded_file)
     extracted_text = pdf_loader.extract_text()
     
-    text_chunks = extracted_text.split('\n\n')  
+    text_chunks = extracted_text.split('\n\n')
+    
     embedding_generator = EmbeddingGenerator()
     embeddings = embedding_generator.generate_embeddings(text_chunks)
     
-
     metadata = [{'pdf_name': uploaded_file.name, 'chunk_number': i} for i in range(len(embeddings))]
-    
     
     store_embeddings(embeddings, metadata)
     
