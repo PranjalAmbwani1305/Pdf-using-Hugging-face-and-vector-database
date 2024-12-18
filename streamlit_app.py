9fb0cfcd0c9886a778d97fd33cbe79609a7f00a9
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2024-12-18 11:21:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,6 @@ os.environ['HUGGINGFACE_API_KEY'] = st.secrets["HUGGINGFACE_API_KEY"]
 os.environ['PINECONE_API_KEY'] = st.secrets["PINECONE_API_KEY"]
 
 
-
-index_name = "textembeddings"
-if index_name not in pinecone.list_indexes():
-    pinecone.create_index(index_name, dimension=1536)
-
-index = pinecone.Index(index_name)
-
 class PDFLoader:
     def __init__(self, pdf_file):
         self.pdf_file = pdf_file
