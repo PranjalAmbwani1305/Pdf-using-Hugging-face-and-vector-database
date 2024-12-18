7d52bc2e1e559ade5d65bd16f1e294783c1bc93d
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2024-12-18 11:28:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,20 +24,17 @@ class PDFLoader:
 
 class EmbeddingGenerator:
     def __init__(self, model_name='all-MiniLM-L6-v2'):
-        self.model = SentenceTransformer(model_name)  # Load SentenceTransformer model
+        self.model = SentenceTransformer(model_name)  
 
     def generate_embeddings(self, text_chunks):
-        return self.model.encode(text_chunks)  # Generate embeddings for text chunks
-
+        return self.model.encode(text_chunks)  
 
 def store_embeddings(embeddings, metadata):
     upsert_data = [
         (f'doc-{i}', embedding.tolist() if isinstance(embedding, np.ndarray) else embedding, metadata[i])
         for i, embedding in enumerate(embeddings) if isinstance(metadata[i], dict)
     ]
-    index.upsert(upsert_data)  # Upsert embeddings and metadata to Pinecone index
-
-
+    upsert(upsert_data) 
 st.title("PDF  Generator")
 
 uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
