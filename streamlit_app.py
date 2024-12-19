5262f944ba7b4b50fc079c15102f0fdef5066d5a
PranjalAmbwani1305 <pranjalambwani13@gmail.com>
2024-12-19 11:13:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ class PDFLoader:
         if self.index_name not in self.pc.list_indexes().names():
             self.pc.create_index(
                 name=self.index_name,
-                dimension=768,  
+                dimension=1536,  
                 metric='cosine',
             )
             st.write(f"Index '{self.index_name}' created.")
